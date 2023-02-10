80e3aad977bddb67ab8dfdc79e5ed23436cd6900
Jan Eglinger <jan.eglinger@fmi.ch>
2023-02-10 09:54:56 +0100

    Allow customizable docstring for Choices enum

--- src/faim_prefect/block/choices.py
+++ src/faim_prefect/block/choices.py
@@ -7,10 +7,10 @@ from prefect.blocks.core import Block
 class Choices(Block):
     """A Prefect block representing a collection of options.
 
-    To register this block follow these instructions:
+    To register this block, follow these instructions:
     https://docs.prefect.io/concepts/blocks/#registering-blocks-for-use-in-the-prefect-ui
 
-    Example
+    Usage example
     -------
     block = Choices.load('groups')
 
@@ -20,7 +20,10 @@ class Choices(Block):
         logger.info(group)
     """
     choices: List[str]
+    docstring: str = 'Select one choice.'
 
     def get(self) -> Enum:
         """Get list of choices."""
-        return Enum('Choices', {x: x for x in self.choices})
+        choices = Enum('Choices', {x: x for x in self.choices})
+        choices.__doc__ = self.docstring
+        return choices
