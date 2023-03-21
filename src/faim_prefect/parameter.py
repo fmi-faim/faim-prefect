8b47d79b61a95573556fbd1765d89516d54c04d8
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2023-03-21 11:15:26 +0100

    Add `User` parameter.

---
+++ src/faim_prefect/parameter.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+from faim_prefect.block.choices import Choices
+
+groups = Choices.load("fmi-groups")
+
+
+class User(BaseModel):
+    name: str
+    group: groups.get()
+    run_name: str
